0faf2cf751467b1f92908d593a032976ba2886f1
Ethan Hackney <ethanhackney@pop-os.localdomain>
2023-12-13 18:40:13 -0800

    implemented normal and insert mode and print first line of a file

--- main.c
+++ main.c
@@ -6,11 +6,25 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
+#include <sys/types.h>
 #include <sysexits.h>
 #include <termios.h>
 #include <unistd.h>
 
 #define CTRL_KEY(k) ((k) & 0x1f)
+
+enum {
+        MOVE_LEFT               = 'h',
+        MOVE_RIGHT              = 'l',
+        MOVE_UP                 = 'k',
+        MOVE_DOWN               = 'j',
+
+        MODE_INSERT             = 0,
+        MODE_NORMAL             = 1,
+
+        ESCAPE                  = 27,
+        ENTER_INSERT_MODE       = 'i',
+};
 
 static void enable_raw_mode(void);
 static void disable_raw_mode(void);
@@ -28,6 +42,7 @@ static void winbuf_puts(char *s);
 static void winbuf_free(void);
 static void winbuf_flush(void);
 static void move(char key);
+static void open_file(char *path);
 
 static struct termios orig_termios;
 static int nr_rows;
@@ -37,13 +52,23 @@ static size_t winbufsize;
 static size_t winbufleng;
 static int cursor_row;
 static int cursor_col;
+static int mode = MODE_NORMAL;
+static char *file_row;
+static int file_rowsize;
+static int nr_lines_in_file;
 
 int
-main(void)
+main(int argc, char **argv)
 {
+        if (argc != 2) {
+                errno = EINVAL;
+                fatal(EX_USAGE, "usage: a.out path");
+        }
+
         get_winsize();
         enable_raw_mode();
 
+        open_file(argv[1]);
         for (;;) {
                 refresh();
                 read_keypress();
@@ -85,20 +110,33 @@ disable_raw_mode(void)
 static void
 read_keypress(void)
 {
-        char c;
+        char c = read_char();
 
-        switch ((c = read_char())) {
-        case CTRL_KEY('q'):
-                clear();
-                exit(0);
+        if (mode == MODE_NORMAL) {
+                switch (c) {
+                case CTRL_KEY('q'):
+                        clear();
+                        exit(0);
 
-        case 'h':
-        case 'l':
-        case 'j':
-        case 'k':
-                move(c);
-                break;
+                case MOVE_UP:
+                case MOVE_DOWN:
+                case MOVE_LEFT:
+                case MOVE_RIGHT:
+                        move(c);
+                        break;
+
+                case 'i':
+                        mode = MODE_INSERT;
+                        break;
+                }
+        } else {
+                switch (c) {
+                case ESCAPE:
+                        mode = MODE_NORMAL;
+                        break;
+                }
         }
+
 }
 
 static char
@@ -167,13 +205,20 @@ fatal(int code, char *fmt, ...)
 static void
 draw(void)
 {
+        size_t len;
         int row;
 
         for (row = 0; row < nr_rows; row++) {
-                winbuf_puts("~");
+                if (row >= nr_lines_in_file) {
+                        winbuf_puts("~");
+                } else {
+                        len = file_rowsize;
+                        if (len > nr_cols)
+                                len = nr_cols;
+                        winbuf_puts(file_row);
+                }
+
                 winbuf_puts("\x1b[K");
-                if (row == nr_rows / 2)
-                        winbuf_puts("\t\t\tHello world!");
                 if (row < nr_rows - 1)
                         winbuf_puts("\r\n");
         }
@@ -240,21 +285,55 @@ static void
 move(char key)
 {
         switch (key) {
-        case 'h':
+        case MOVE_LEFT:
                 if (cursor_col)
                         cursor_col--;
                 break;
-        case 'l':
+        case MOVE_RIGHT:
                 if (cursor_col < nr_cols)
                         cursor_col++;
                 break;
-        case 'k':
+        case MOVE_UP:
                 if (cursor_row)
                         cursor_row--;
                 break;
-        case 'j':
+        case MOVE_DOWN:
                 if (cursor_row < nr_rows)
                         cursor_row++;
                 break;
         }
+}
+
+static void
+open_file(char *path)
+{
+        FILE *fp;
+        char *line;
+        size_t linecap;
+        ssize_t linelen;
+        int end;
+
+        if (!(fp = fopen(path, "r")))
+                fatal(EX_SOFTWARE, "open_file(): fopen()");
+
+        linecap = 0;
+        linelen = getline(&line, &linecap, fp);
+        if (linelen != -1) {
+                end = line[linelen - 1];
+                if (end == '\n' || end == '\r')
+                        line[--linelen] = '\0';
+
+                file_rowsize = linelen;
+                file_row = malloc(linelen + 1);
+                if (!file_row)
+                        fatal(EX_SOFTWARE, "open_file(): malloc()");
+
+                memcpy(file_row, line, linelen);
+                file_row[linelen] = '\0';
+                nr_lines_in_file = 1;
+        }
+        free(line);
+
+        if (fclose(fp))
+                fatal(EX_SOFTWARE, "open_file(): fclose()");
 }
